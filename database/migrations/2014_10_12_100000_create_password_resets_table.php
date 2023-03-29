d1c8abe67f34508860d1134df23dd125785b34d0
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-03-29 10:53:15 +0800

    remove primary from create_password_resets_table

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->text('email')->primary();
+            $table->text('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
